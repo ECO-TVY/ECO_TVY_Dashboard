70c21751c4a71dc4300631246c79c0587035e0ab
[RosieOh] <[20172207@gm.hannam.ac.kr]>
2024-02-05 01:42:47 +0900

    ♻️build : 프로젝트 초기 세팅 작업 #1♻️

---
+++ src/main/java/com/ecotvy/EcotvyApplication.java
@@ -0,0 +1,13 @@
+package com.ecotvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotvyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcotvyApplication.class, args);
+    }
+
+}
